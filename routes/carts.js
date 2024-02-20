95f59d9e65c8022a553ad1dc684ec734c2c297ba
kimhongrae <khr0690@hanmail.net>
2024-02-21 03:33:43 +0900

    라우터 수정

--- routes/carts.js
+++ routes/carts.js
@@ -1,28 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+} = require("../controller/CartController");
 
 router.use(express.json());
 
-router.post("/", (req, res) => {
-  res.json({
-    message: "장바구니 담기",
-  });
-});
-
-router.get("/", (req, res) => {
-  res.json({
-    message: "장바구니 조회",
-  });
-});
-router.delete("/:id", (req, res) => {
-  res.json({
-    message: "장바구니 도서 삭제",
-  });
-});
-// router.get("/", (req, res) => {
-//   res.json({
-//     message: "장바구니에서 선택한 주문 예상 상품 목록 조회",
-//   });
-// });
+router.post("/", addToCart);
+router.get("/", getCartItems);
+router.delete("/:id", removeCartItem);
 
 module.exports = router;
--- routes/likes.js
+++ routes/likes.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { addLike, removeLike } = require("../controller/LikeController");
 
 router.use(express.json());
 
-router.post("/:id", (req, res) => {
-  res.json({
-    message: "좋아요 추가",
-  });
-});
+router.post("/:id", addLike); //좋아요 추가
 
-router.delete("/:id", (req, res) => {
-  res.json({
-    message: "좋아요 삭제",
-  });
-});
+router.delete("/:id", removeLike); // 좋아요 삭제
 
 module.exports = router;
