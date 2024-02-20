5bfa31829ff3e191f2211fd7ff6da127e445aca5
kimhongrae <khr0690@hanmail.net>
2024-02-21 03:34:16 +0900

    CartController 모듈 추가 및 코드 수정

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,50 +1,91 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const ensureAuthorization = require("../auth");
 
 //장바구니 담기
 const addToCart = (req, res) => {
-  const { book_id, quantity, user_id } = req.body;
-  let sql = "INSERT INTO cartItems (book_id,quantity, user_id) VALUES (?,?,?)";
-  let values = [book_id, quantity, user_id];
+  const { book_id, quantity } = req.body;
+  let authorization = ensureAuthorization(req, res);
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.OK).json(results);
-  });
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인 세션이 만료되었습니다. ",
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "로그인 토큰이 유효하지 않습니다. ",
+    });
+  } else {
+    let sql =
+      "INSERT INTO cartItems (book_id,quantity, user_id) VALUES (?,?,?)";
+    let values = [book_id, quantity, authorization.id];
+
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 //장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { selected } = req.body;
+  let authorization = ensureAuthorization(req, res);
 
-  let sql =
-    "SELECT cartItems.id, book_id, title, summary, quantity, price FROM cartItems LEFT JOIN books ON cartItems.book_id = books.id WHERE user_id = ? AND cartItems.id IN(?)";
-
-  let values = [user_id, selected];
-
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      return res.status(StatusCodes.BAD_REQUEST).end();
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인 세션이 만료되었습니다. ",
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "로그인 토큰이 유효하지 않습니다. ",
+    });
+  } else {
+    let sql =
+      "SELECT cartItems.id, book_id, title, summary, quantity, price FROM cartItems LEFT JOIN books ON cartItems.book_id = books.id WHERE user_id = ?";
+    let values = [authorization.id];
+    if (selected) {
+      sql += " AND cartItems.id IN(?)";
+      values.push(selected);
     }
-    return res.status(StatusCodes.OK).json(results);
-  });
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 //장바구니 도서 삭제
 const removeCartItem = (req, res) => {
-  let { id } = req.params; //cartItemId
+  let authorization = ensureAuthorization(req, res);
 
-  let sql = "DELETE FROM cartItems WHERE id = ?";
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인 세션이 만료되었습니다. ",
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "로그인 토큰이 유효하지 않습니다. ",
+    });
+  } else {
+    let cartItemId = req.params.id; //cartItemId
 
-  conn.query(sql, id, (err, results) => {
-    if (err) {
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.OK).json(results);
-  });
+    let sql = "DELETE FROM cartItems WHERE id = ?";
+
+    conn.query(sql, cartItemId, (err, results) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
+
 module.exports = {
   addToCart,
   getCartItems,
